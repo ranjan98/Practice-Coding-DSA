5a819676d2e9da83eb3dd51dbb25b2a844c7a2df
Ron <ranjangoyal98@gmail.com>
2021-06-27 19:26:56 +0530

    Update MergeTwoSortedArrays.java

--- MergeTwoSortedArrays.java
+++ MergeTwoSortedArrays.java
@@ -1,5 +1,4 @@
-// Merge Two Sorted Arrays
-public class MergeTwoSortedArrays{
+public class HelloWorld{
     public static int[] mergeSortedArrays(int[] arr1, int[] arr2){
         // Maintain an array for Result
         int mergedArr[] = new int[arr1.length + arr2.length];
@@ -33,7 +32,7 @@ public class MergeTwoSortedArrays{
             }
         }
         else if(j<arr2.length){
-            while(j<arr1.length){
+            while(j<arr2.length){
                 mergedArr[index++] = arr2[j];
                 j++;
             }
@@ -43,7 +42,7 @@ public class MergeTwoSortedArrays{
     public static void main(String []args){
         // Merge 2 Sorted Arrays
         int arr1[] = new int[]{ 1, 3, 6, 8};
-        int arr2[] = new int[]{ 0, 4, 5};
+        int arr2[] = new int[]{ 0, 4, 5, 9, 12};
         int mergedArr[] = mergeSortedArrays(arr1, arr2);
         for(int i=0;i<mergedArr.length;i++){
             System.out.print(mergedArr[i]+ " ");
